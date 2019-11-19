866ca3c8b7c047b5ccf3b4793a4e7669bfe4c8d7
sangminLee <nalsm98@naver.com>
2019-11-20 00:03:36 +0900

    팩토리 메서드 공부(100%)

--- src/factory_method_pattern/Client.java
+++ src/factory_method_pattern/Client.java
@@ -5,17 +5,17 @@ import template_method_pattern.unused_pattern.Direction;
 public class Client {
 
   public static void main(String[] args) {
-    ElevatorManager emWithResponseTimeScheduler =
-        new ElevatorManager(2, SchedulingStrategyID.RESPONSE_TIME);
-    emWithResponseTimeScheduler.requestElevator(10 , Direction.UP);
-
-    ElevatorManager emWithThroughoutScheduler =
-        new ElevatorManager(2, SchedulingStrategyID.THROUGHPUT);
-    emWithThroughoutScheduler.requestElevator(10, Direction.UP);
-
-    ElevatorManager emWithDynamicScheduler =
-        new ElevatorManager(2, SchedulingStrategyID.DYNAMIC);
-    emWithDynamicScheduler.requestElevator(10, Direction.UP);
+//    ElevatorManager emWithResponseTimeScheduler =
+//        new ElevatorManager(2, SchedulingStrategyID.RESPONSE_TIME);
+//    emWithResponseTimeScheduler.requestElevator(10 , Direction.UP);
+//
+//    ElevatorManager emWithThroughoutScheduler =
+//        new ElevatorManager(2, SchedulingStrategyID.THROUGHPUT);
+//    emWithThroughoutScheduler.requestElevator(10, Direction.UP);
+//
+//    ElevatorManager emWithDynamicScheduler =
+//        new ElevatorManager(2, SchedulingStrategyID.DYNAMIC);
+//    emWithDynamicScheduler.requestElevator(10, Direction.UP);
   }
 
 }
